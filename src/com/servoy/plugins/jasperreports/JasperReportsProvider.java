03a7e4eda44e2551aa85007a960fd0e53a645dca
Gabi Boros <gboros@servoy.com>
2023-01-23 12:27:50 +0200

    SVY-17669 Jasper Reports Plugin Functions fail

--- src/com/servoy/plugins/jasperreports/JasperReportsProvider.java
+++ src/com/servoy/plugins/jasperreports/JasperReportsProvider.java
@@ -1560,7 +1560,7 @@ public class JasperReportsProvider implements IScriptable, IReturnedTypesProvide
 	 */
 	public String js_getPluginVersion()
 	{
-		return "6.19.1";
+		return "6.20.0";
 
 		/*
 		 * Added destination optional parameter for compileReport method Renamed jasperReport -> runReport, jasperCompile -> compileReport, readFile ->
